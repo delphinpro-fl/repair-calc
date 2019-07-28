e99a0c54c0eba49038dfb1d5706d120822561032
delphinpro <delphinpro@yandex.ru>
2019-07-28 15:11:18 +0400

    Настройка AppMenu

--- src/store.js
+++ src/store.js
@@ -12,7 +12,52 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state    : {},
-    mutations: {},
-    actions  : {},
+    state: {
+        isOpenMainMenu: false,
+
+        mainmenu: [
+            {
+                title: 'Бесплатная консультация',
+                url  : '#',
+                icon : 'IconConsulting',
+            },
+            {
+                title: 'Заказать замер',
+                url  : '#',
+                icon : 'IconSquare',
+            },
+            {
+                title: 'Скачать каталог',
+                url  : '#',
+                icon : 'IconDownload',
+            },
+            {
+                title: 'О компании',
+                url  : '#',
+                icon : 'IconSign',
+            },
+        ],
+
+        telephone: '+7 (495) 000 00 00',
+        instagram: '#',
+
+        companyInfo  : '<p>ООО «Аквариус» &nbsp;&nbsp;&nbsp; ОГРН 0000000000000</p>',
+        privacyPolicy: '#',
+        termOfUse    : '#',
+    },
+
+    getters: {
+        telephoneAsLink(state) {
+            let plus = state.telephone.length && state.telephone[0] === '+' ? '+' : '';
+            return `tel:${plus}${state.telephone.replace(/\D/g, '')}`;
+        },
+    },
+
+    mutations: {
+        mainmenuToggle(state) {
+            state.isOpenMainMenu = !state.isOpenMainMenu;
+        },
+    },
+
+    actions: {},
 });
