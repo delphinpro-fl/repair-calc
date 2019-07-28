7831ef641ea0e6434986ce585b45d0ff5e87b39a
delphinpro <delphinpro@yandex.ru>
2019-07-28 13:12:15 +0400

    Добавить иконки для меню

---
+++ src/components/Icon/IconConsulting.vue
@@ -0,0 +1,20 @@
+<script>/*!
+ * Аквариус
+ * File: IconConsulting.vue
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'IconConsulting',
+};
+</script>
+
+<template functional>
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+        <g fill="currentColor">
+            <path d="M256,154.1V3.2H45.7v32h169.1c2.5,0,4.6,2,4.6,4.6v114.3H256z"></path>
+            <path d="M101.9,196.6c0.9-0.9,2-1.3,3.2-1.3h105.1V44.4H0v150.9h41.1c2.5,0,4.6,2,4.6,4.6v53L101.9,196.6z"></path>
+        </g>
+    </svg>
+</template>
---
+++ src/components/Icon/IconSign.vue
@@ -0,0 +1,19 @@
+<script>/*!
+ * Аквариус
+ * File: IconSign.vue
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'IconSign',
+};
+</script>
+
+<template functional>
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+        <g fill="currentColor">
+            <path d="M159.1,0c17.1,0,25.7,11.7,25.7,25.1c0,16.7-14.9,32.1-34.3,32.1c-16.2,0-25.7-9.6-25.3-25.5 C125.3,18.4,136.6,0,159.1,0z M106.4,256c-13.5,0-23.5-8.4-14-45.1l15.5-65.2c2.7-10.4,3.1-14.6,0-14.6c-4.1,0-21.6,7.2-32,14.3 l-6.8-11.3c32.9-28,70.8-44.4,87.1-44.4c13.5,0,15.8,16.3,9,41.3l-17.8,68.5c-3.2,12.1-1.8,16.3,1.3,16.3c4.1,0,17.4-5,30.5-15.5 l7.7,10.4C154.8,243.5,119.9,256,106.4,256z"></path>
+        </g>
+    </svg>
+</template>
