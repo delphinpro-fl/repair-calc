1d1296302c008519dcd897a3666295be617137a8
delphinpro <delphinpro@yandex.ru>
2019-07-28 14:40:29 +0400

    Добавлен компонент BurgerButton.vue

---
+++ src/components/BurgerButton.vue
@@ -0,0 +1,33 @@
+<script>/*!
+ * Аквариус
+ * File: BurgerButton.vue
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'BurgerButton',
+
+    methods: {
+        toggleMenu() {
+            this.$store.commit('mainmenuToggle');
+        },
+    },
+};
+</script>
+
+<template>
+    <button
+        class="burger-button"
+        :class="{'burger-button_close':$store.state.isOpenMainMenu}"
+        type="button"
+        @click="toggleMenu()"
+    >
+        <span class="burger-button__box">
+            <span class="burger-button__line burger-button__line_top"></span>
+            <span class="burger-button__line burger-button__line_btm"></span>
+        </span>
+    </button>
+</template>
+
+<style lang="scss" src="../assets/scss/components/BurgerButton.scss"></style>
