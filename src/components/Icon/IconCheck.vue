b3ad82059b22fb3c8e2f05240ec277e2ff1abbaf
delphinpro <delphinpro@yandex.ru>
2019-07-28 15:24:36 +0400

    Добавлен компонент IconCheck.vue

---
+++ src/components/Icon/IconCheck.vue
@@ -0,0 +1,19 @@
+<script>/*!
+ * Аквариус
+ * File: IconCheck.vue
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'IconCheck',
+};
+</script>
+
+<template functional>
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+        <g fill="currentColor">
+            <polygon points="81.5,184.7 20.4,123.6 0,144 81.5,225.5 256,50.9 235.6,30.5"></polygon>
+        </g>
+    </svg>
+</template>
