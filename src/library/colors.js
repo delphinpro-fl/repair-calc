5a24b5aa8d51f1b56fc2820f46277a52d2e3e290
delphinpro <delphinpro@yandex.ru>
2019-07-29 14:32:11 +0400

    Добвлен список цветов для раскраски интерфейса

---
+++ src/library/colors.js
@@ -0,0 +1,16 @@
+/*!
+ * Аквариус
+ * File: colors.js
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export const colors = [
+    '#ff0048',
+    '#a701ff',
+    '#007cfe',
+    '#ff8c00',
+    '#01c4ff',
+    '#01e676',
+    '#e1e61a',
+];
