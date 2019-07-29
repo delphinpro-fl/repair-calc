cdf46a05076a740d343af0b94f65b29060fc7cc3
delphinpro <delphinpro@yandex.ru>
2019-07-29 14:55:03 +0400

    Добавлен компонент OptionRow.vue

---
+++ src/components/OptionRow.vue
@@ -0,0 +1,34 @@
+<script>/*!
+ * Аквариус
+ * File: OptionRow.vue
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'OptionRow',
+
+    props: {
+        label  : String,
+        id     : String,
+        reverse: Boolean,
+    },
+
+    computed: {
+        labelTag() {
+            return this.id ? 'label' : 'div';
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="option-row" :class="{'option-row_reverse':reverse}">
+        <component class="option-row__label" :is="labelTag" :for="id">{{label}}</component>
+        <div class="option-row__control">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" src="../assets/scss/components/OptionRow.scss"></style>
