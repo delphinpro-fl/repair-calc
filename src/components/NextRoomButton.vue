f0f8688e0777bf04ff811e10131904cae1273b00
delphinpro <delphinpro@yandex.ru>
2019-07-29 23:33:35 +0400

    Добавлен компонент NextRoomButtons.vue

---
+++ src/components/NextRoomButton.vue
@@ -0,0 +1,25 @@
+<script>/*!
+ * Аквариус
+ * File: NextRoomButton.vue
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'NextRoomButton',
+
+    props: {
+        color: String,
+    },
+};
+</script>
+
+<template>
+    <button
+        class="next-room-button"
+        :style="{color}"
+        @click="$emit('click', $event)"
+    ><span class="next-room-button__text">Перейти к следующей комнате</span></button>
+</template>
+
+<style lang="scss" src="../assets/scss/components/NextRoomButton.scss"></style>
