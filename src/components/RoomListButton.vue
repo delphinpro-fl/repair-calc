bfeff125d5bf4dcbf73898e3ed1d50cc22dc1e90
delphinpro <delphinpro@yandex.ru>
2019-07-29 23:35:04 +0400

    Добавлен компонент RoomListButton.vue

---
+++ src/components/RoomListButton.vue
@@ -0,0 +1,36 @@
+<script>/*!
+ * Аквариус
+ * File: RoomListButton.vue
+ * (c) 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+import IconCheck from '@/components/Icon/IconCheck';
+
+
+export default {
+    name: 'RoomListButton',
+
+    props: {
+        isActive  : Boolean,
+        isComplete: Boolean,
+        title     : String,
+        color     : String,
+    },
+
+    components: {
+        IconCheck,
+    },
+};
+</script>
+
+<template>
+    <button class="room-list-button" :class="{isActive}" type="button">
+        <span class="dot" :class="{isComplete}" :style="{color}">
+            <IconCheck/>
+        </span>
+        <span v-text="title"></span>
+    </button>
+</template>
+
+<style lang="scss" src="../assets/scss/components/RoomListButton.scss"></style>
