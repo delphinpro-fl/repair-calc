796cff7b0f41bdc191053946c1414d6ac51be026
delphinpro <delphinpro@yandex.ru>
2019-08-25 18:04:58 +0400

    Добавлен компонент ToggleButton.vue

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,36 @@
+<script>/*!
+ * Аквариус
+ * File: ToggleButton.vue
+ * © 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+import IconAngle from '@/components/Icon/IconAngle';
+
+
+export default {
+    name: 'ToggleButton',
+
+    components: {
+        IconAngle,
+    },
+
+    props: {
+        isOpen: { type: Boolean, default: false },
+    },
+
+    computed: {
+        vectorIcon() {
+            return this.isOpen ? 'bottom' : 'right';
+        },
+    },
+};
+</script>
+
+<template>
+    <button class="toggle-button" @click="$emit('click', $event)">
+        <IconAngle class="toggle-button__icon" :vector="vectorIcon"/>
+    </button>
+</template>
+
+<style lang="scss" src="../assets/scss/components/ToggleButton.scss"></style>
