c657dca7594be9f8c3bfb864d2eb13d2ace22bad
delphinpro <delphinpro@yandex.ru>
2019-08-26 13:37:11 +0400

    Простой компонент уведомлений

---
+++ src/components/App/AppToast.vue
@@ -0,0 +1,27 @@
+<script>/*!
+ * Аквариус
+ * File: AppToast.vue
+ * © 2019 delphinpro <delphinpro@gmail.com>
+ * licensed under the MIT license
+ */
+
+export default {
+    name: 'AppToast',
+
+    props: {
+        msg: { type: String, default: '' },
+    },
+
+    methods: {
+        hideMe() {
+            this.$store.commit('hideToast');
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="toast" :class="{show: !!msg}" v-html="msg" @click="hideMe"></div>
+</template>
+
+<style lang="scss" src="../../assets/scss/components/AppToast.scss"></style>
